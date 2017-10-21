a4a38cc6b2f64652b5e99fc385f96943637e136b
cdiaz19 <cristian.diaz.jim@gmail.com>
2017-10-21 13:19:37 -0600

    AVL project

--- proyectoAVL/src/proyectoavl/ProyectoAVL.java
+++ proyectoAVL/src/proyectoavl/ProyectoAVL.java
@@ -3,28 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectobingo;
+package proyectoavl;
 
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-import proyectobingo.controller.Controller;
-import proyectobingo.model.Stack;
+import proyectoavl.model.AVLTree;
+import proyectoavl.model.Book;
 
 /**
  *
  * @author cristian
  */
-public class ProyectoBingo {
+public class ProyectoAVL {
 
     /**
      * @param args the command line arguments
      */
-       
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Controller ctl = new Controller();
-               
-    }
+        AVLTree<String> tree = new AVLTree<>();
+        for (int i = 1; i <= 7; i++) {
+            tree.insert(String.valueOf(new Book("Name", "Code", "Author", "Description", i)));
+            System.err.println(i);
+        }
+
+        tree.PrintTree();
+        }
 }
---
+++ proyectoAVL/src/proyectoavl/model/Book.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoavl.model;
+
+/**
+ *
+ * @author cristian
+ */
+public class Book {
+
+    private String name;
+    private String code;
+    private String author;
+    private String description;
+    private int type;
+
+    public Book() {
+    }
+
+    public Book(String name, String code, String author, String description, int type) {
+        this.name = name;
+        this.code = code;
+        this.author = author;
+        this.description = description;
+        this.type = type;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the type
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "name=" + name + '}';
+    }
+}
